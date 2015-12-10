bc4eaf39244cdf031f6582ce63093336c3c83dd7
andresosw jiemenz <andres_osw@hotmail.com>
2015-12-10 17:28:37 -0430

    added app_name to urls to add namespace to template url backref

--- floristeriaSite/floristeriaSite/urls.py
+++ floristeriaSite/floristeriaSite/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url
 from django.contrib import admin
 import views
 
+app_name = 'floristeriaSite'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.home,name='home')
